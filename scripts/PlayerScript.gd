f528d4b32a008e4fa3ccb7bd85165e3feadb599f
aliamanuba <31815005+TuxPenguin09@users.noreply.github.com>
2021-11-03 20:17:52 +0800

    Adding Level and Esteem system

--- scripts/PlayerScript.gd
+++ scripts/PlayerScript.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 # Main Status
-var levelxp : int = 0 # max 26540 - Lvl. 100
+var level : int = 1
+var levelxp : int = 0 # (Level Int * 16 = levelxp)
 var karma : float = 0.0
 
 # Currency £
@@ -11,8 +12,10 @@ var bank : float = 512.0
 # Play Time
 var playTime : float = 0.0 # Hours.Minutes
 var playTimeSeconds : int = 0.0 # Seconds
+# This will delete your save if modified and its based on the levelxp
 
 
 # Player Status
 var gender : String = "Male" # Gender, [ Male, Female ]
 var personality : String = "Normal" # Permanent Option at Save
+var esteem : float = 0.0 # Confidential
