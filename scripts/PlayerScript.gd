3116c89d97052780d010a83de90e406ddd77b47e
aliamanuba <31815005+TuxPenguin09@users.noreply.github.com>
2021-11-03 13:17:47 +0800

    Adding a main mechanic vars

--- scripts/PlayerScript.gd
+++ scripts/PlayerScript.gd
@@ -1,2 +1,13 @@
 extends KinematicBody2D
 
+# Main Status
+var levelxp : int = 0
+var karma : float = 0.0
+
+# Currency
+var money : float = 20.0
+var bank : float = 512.0
+
+# Play Time
+var playTime : float = 0.0 # Hours.Minutes
+var playTimeSeconds : int = 0.0 # Seconds
