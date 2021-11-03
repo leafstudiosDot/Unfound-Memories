3e6e5950b3852effb37d84ff6b4ec6d665f31ab2
aliamanuba <31815005+TuxPenguin09@users.noreply.github.com>
2021-11-03 13:32:38 +0800

    Player Status

---
+++ scripts/Notes/PersonalityGuide.gd
@@ -0,0 +1,20 @@
+"""
+"Normal": {
+	Karma: 5,
+	Description: "
+		Just acting normal
+	"
+},
+"Stubborn": {
+	Karma: -10,
+	Description: "
+		You aren't alright and always mad at things.
+	"
+},
+"Loveable": {
+	Karma: 15,
+	Description: "
+		Everyone loves you <3
+	"
+}
+"""
--- scripts/PlayerScript.gd
+++ scripts/PlayerScript.gd
@@ -1,13 +1,18 @@
 extends KinematicBody2D
 
 # Main Status
-var levelxp : int = 0
+var levelxp : int = 0 # max 26540 - Lvl. 100
 var karma : float = 0.0
 
-# Currency
+# Currency £
 var money : float = 20.0
 var bank : float = 512.0
 
 # Play Time
 var playTime : float = 0.0 # Hours.Minutes
 var playTimeSeconds : int = 0.0 # Seconds
+
+
+# Player Status
+var gender : String = "Male" # Gender, [ Male, Female ]
+var personality : String = "Normal"
