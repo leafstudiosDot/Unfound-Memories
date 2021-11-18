3a2de20bbf5d9179e7beb60c940247491de628d2
Allen David <allendavid214@gmail.com>
2021-11-18 20:33:36 +0800

    Changed Text Flow
    Made the text flow looks smoother

--- scenes/prototype/Text.gd
+++ scenes/prototype/Text.gd
@@ -29,5 +29,5 @@ func _input(event):
 
 
 func _on_Timer_timeout():
-	set_visible_characters(get_visible_characters()+3)
+	set_visible_characters(get_visible_characters()+1)
 	pass # Replace with function body.
