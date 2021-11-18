4f2955f945c01a62bf5891ed0ac8de9cbdfd61d6
aliamanuba <31815005+TuxPenguin09@users.noreply.github.com>
2021-11-19 07:01:14 +0800

    Male Walking Done

--- scripts/MainMenuInterface/PlayerScript.gd
+++ scripts/MainMenuInterface/PlayerScript.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 # Main Status
 var online = false
+var female = false
+var flipped = false
 var level : int = 1
 var levelxp : int = 0 # (Level Int * 16 = levelxp)
 var karma : float = 0.0
@@ -31,6 +33,8 @@ onready var rayCast = get_node("RayCast2D")
 onready var playerNameLabel = get_node("Player_Name")
 
 func _ready():
+	$AnimationPlayer.play("idle_right")
+	
 	if online:
 		playerNameLabel.show()
 	else:
@@ -39,20 +43,36 @@ func _ready():
 func _physics_process(delta):
 	loc_coord = Vector2()
 	
+	get_node( "Sprite_Male" ).set_flip_h( flipped )
+	
 	# Inputs and Actions
 	if Input.is_action_pressed("playermove_up"):
+		$AnimationPlayer.play("walk_right")
 		loc_coord.y -= moveSpeed + speedBonus
+	elif Input.is_action_just_released("playermove_up"):
+		$AnimationPlayer.play("idle_right")
 	
 	if Input.is_action_pressed("playermove_down"):
+		$AnimationPlayer.play("walk_right")
 		loc_coord.y += moveSpeed + speedBonus
+	elif Input.is_action_just_released("playermove_down"):
+		$AnimationPlayer.play("idle_right")
 		
 	if Input.is_action_pressed("playermove_left"):
+		flipped = true
+		$AnimationPlayer.play("walk_right")
 		loc_coord.x -= moveSpeed + speedBonus
 		facing = Vector2(-1, 0)
+	elif Input.is_action_just_released("playermove_left"):
+		$AnimationPlayer.play("idle_right")
 		
 	if Input.is_action_pressed("playermove_right"):
+		flipped = false
+		$AnimationPlayer.play("walk_right")
 		loc_coord.x += moveSpeed + speedBonus
 		facing = Vector2(1, 0)
+	elif Input.is_action_just_released("playermove_right"):
+		$AnimationPlayer.play("idle_right")
 		
 	if Input.is_action_pressed("sprint"):
 		speedBonus = 15
