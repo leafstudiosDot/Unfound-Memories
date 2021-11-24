b55d68ace7a1ac81cdf6f4b163bf1ddb89863a3a
aliamanuba <31815005+TuxPenguin09@users.noreply.github.com>
2021-11-24 09:35:20 +0800

    Resizing Images

--- scenes/Home1.gd
+++ scenes/Home1.gd
@@ -7,6 +7,7 @@ var weaponEquiped
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_node("StaticBody2D").visible = true
 	pass # Replace with function body.
 
 
